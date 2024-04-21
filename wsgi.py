97a76b0db4bf456719ef2e80c598736a5e74ff9e
Khant-SoDOpe <khantzay.ly@gmail.com>
2024-04-21 12:31:01 +0700

    rebuild

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from main import app  # Assuming your Flask application instance is named 'app' in main.py
-
-if __name__ == "__main__":
-    app.secret_key = "handsomeKhant"
-    app.run(host='0.0.0.0', port=3000)
