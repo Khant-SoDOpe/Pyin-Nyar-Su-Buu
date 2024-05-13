25eaa4d5c17447fdfbba7fff029836080bf9537a
Sai Khant Zay Lynn Yaung(Andrew) <khantzay.ly@gmail.com>
2024-05-13 11:15:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from routes.google import google_bp
 # Initialize Flask app
 app = Flask(__name__)
 load_dotenv()
-app.secret_key = "something"
+app.secret_key = os.getenv("SECRET_KEY")
 # Routes
 
 @app.route("/")
